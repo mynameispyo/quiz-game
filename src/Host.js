2fb7de6900def4c31cc84307d2efd53133f1b82e
Inpyo Lee <mynameispyo@gmail.com>
2020-10-25 17:09:40 -0400

    made host access

--- src/Host.js
+++ src/Host.js
@@ -6,6 +6,7 @@ export default function Host() {
     const [init, setInit ] = useState(null);
     const [userObj, setUserObj] = useState(null);
     const [myData, setMyData] = useState({});
+    const [userAcces, setUserAccess] = useState(false);
     const gameId = new URLSearchParams(useLocation().search).get('gameId');
     useEffect(()=>{
         authService.onAuthStateChanged((user)=>{
@@ -14,29 +15,25 @@ export default function Host() {
                     displayName: user.displayName,
                     uid: user.uid,
                 });
+                checkRightUser(user.uid);
             }else{
                 setUserObj(null);
             }
             setInit(true);
         })
     }, []);
-    const checkRightUser = () => {
-        let result = false;
-        myData.project.projects.forEach(quiz => {
-            if(quiz.gameId == gameId){
-                result = true;
-            }
-        })
-        
-    }
-    const loadDB = async () => {
-        var docRef = await dbService.doc("users/"+userObj.uid);
-  
+    const checkRightUser = async (uid) => {
+        var docRef = dbService.doc("users/"+uid);
+
         docRef.get().then(function(doc) {
             if (doc.exists) {
-                setMyData(doc.data().project.projects);
+                doc.data().project.projects.forEach(quiz => {
+                    if(quiz.gameId == gameId){
+                        setUserAccess(true);
+                    }
+                })
             } else {
-                console.log("fatal error occur");
+                setUserAccess(false);
             }
         })
     }
@@ -45,7 +42,11 @@ export default function Host() {
             {
                 init? (
                     Boolean(userObj) ? (
-                        <p>{gameId}</p>
+                        userAcces ? (
+                            <p>{gameId}</p>
+                        ) : (
+                            <p>You don't have access in this account</p>
+                        )
                     ) :(
                         <Login />
                     )
